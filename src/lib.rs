d314462ef03b6f78d3c0379f1233021851f342a8
Yuheng Su <gipsyh.icu@gmail.com>
2024-02-01 09:00:16 +0800

    update

--- src/lib.rs
+++ src/lib.rs
@@ -184,7 +184,7 @@ impl Solver {
     ) -> SatResult<'_> {
         self.new_round(Some(domain));
         self.statistic.num_solve += 1;
-        if self.statistic.num_solve % 100 == 0 {
+        if self.statistic.num_solve % 100 == 1 {
             self.simplify();
         }
         if self.search(assumption) {
